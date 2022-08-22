28008a53737421348357d3c01117e845a650051a
Franco-ortellado <ortellado.frank@gmail.com>
2022-08-22 10:40:56 -0300

    update

--- Modulo_2/Ejercicio_1.js
+++ Modulo_2/Ejercicio_1.js
@@ -2,7 +2,12 @@ const nombre = "Franco";
 const edad = 25;
 const desarrollador = false;
 const nacimiento = new Date("may 25 1997");
-const libro = "El Alquimista";
+const libro = {
+  titulo: "El Alquimista",
+  Autor: "Paulo Coelho",
+  fecha: 1988,
+  url: "https://www.amazon.es/Alquimista-Biblioteca-Bolsillo-Paulo-Coelho/dp/8408253107/ref=sr_1_1?adgrpid=120751627651&hvadid=486231123086&hvdev=c&hvlocphy=20009&hvnetw=g&hvqmt=b&hvrand=28395312393976532&hvtargid=kwd-315122692449&hydadcr=14580_1802849&keywords=the+alchemist+amazon&qid=1661175336&sr=8-1",
+};
 
 const lista = [nombre, edad, desarrollador, nacimiento, libro];
 
