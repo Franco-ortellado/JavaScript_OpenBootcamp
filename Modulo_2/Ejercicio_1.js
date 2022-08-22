7601635f2812cbc806c6f760ec11fdc606a8a1d3
Franco-ortellado <ortellado.frank@gmail.com>
2022-08-22 10:27:03 -0300

    update

---
+++ Modulo_2/Ejercicio_1.js
@@ -0,0 +1,9 @@
+const nombre = "Franco";
+const edad = 25;
+const desarrollador = false;
+const nacimiento = new Date("may 25 1997");
+const libro = "El Alquimista";
+
+const lista = [nombre, edad, desarrollador, nacimiento, libro];
+
+console.log(lista);
