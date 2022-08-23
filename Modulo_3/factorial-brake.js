b1c7aa6b56f60e21e2a4cbe33651d48aac23c3cf
Franco-ortellado <ortellado.frank@gmail.com>
2022-08-23 11:01:20 -0300

    update

---
+++ Modulo_3/factorial-brake.js
@@ -0,0 +1,11 @@
+let factorial = 1;
+i = 1;
+
+while (i <= 10) {
+  if (i <= 10) {
+    factorial *= i;
+    i++;
+  }
+}
+
+console.log(factorial);
---
+++ Modulo_3/factorial-for.js
@@ -0,0 +1,7 @@
+factorial = 1;
+
+for (let i = 1; i <= 10; i++) {
+  factorial *= i;
+}
+
+console.log(factorial);
