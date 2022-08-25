98936d60d42aaec17a069d68f8fc2f9f273b1f04
Franco-ortellado <ortellado.frank@gmail.com>
2022-08-25 10:04:09 -0300

    update

---
+++ Modulo_5/Ejercicio1.js
@@ -0,0 +1,11 @@
+let altura_cm = 175;
+
+let altura_mts = altura_cm / 100;
+
+let peso_kg = 74.3;
+
+let altera_mts_redondo = 2;
+
+let peso_redon_abajo = parseInt(peso_kg);
+
+let max_valor = Number.MAX_VALUE + 1 == Number.MAX_VALUE;
