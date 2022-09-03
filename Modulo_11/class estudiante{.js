bc3de75b317b2148661aee4955d4827915110d45
Franco-ortellado <ortellado.frank@gmail.com>
2022-09-03 17:33:47 -0300

    update

---
+++ Modulo_11/class estudiante{.js
@@ -0,0 +1,13 @@
+class estudiante {
+  nombre = "Franco";
+  asignaturas = ["JavaScript", "HTML", "CSS"];
+
+  obtenDatos() {
+    this.nombre;
+    this.asignaturas;
+  }
+}
+
+const nuevo_Estudiante = new estudiante();
+
+console.log(nuevo_Estudiante);
